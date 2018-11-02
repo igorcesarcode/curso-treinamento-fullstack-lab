ccac64af14b95342b8075c55e08f731cf8235dcd
igorcesarcode <igorcesarbernades@gmail.com>
2018-11-02 17:35:11 -0300

    Primeiro arquivo para testar

---
+++ test1.js
@@ -0,0 +1,13 @@
+console.log('Olha Fullstack Lab')
+
+let a = 10;
+const repetidor = 10;
+if (a>20){
+    //verdadeiro
+}else{
+    //falso
+}
+
+for (let i=0; i<repetidor; i++){
+    console.log('Repetidor número:',i);
+}
