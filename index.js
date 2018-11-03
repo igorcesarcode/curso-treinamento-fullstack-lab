4a7f5100552a64ef78f0cdb7093c5735f1cf38a0
igorcesarcode <igorcesarbernades@gmail.com>
2018-11-02 22:13:44 -0300

    Foi adicionado o EJS no projeto como Render View

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require ('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 //Pegando a porta do ambinte caso não consiga ela inicia na porta 3000
 const port = process.env.PORT  || 3000
 
 app.get('/', (request, response) => {
-    response.send ('<h1>Olá Fullstack Lab<h1>')
+    response.render('index')
 })
 
 app.listen(port, (err) => {
