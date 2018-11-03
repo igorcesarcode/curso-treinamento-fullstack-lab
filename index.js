6232fcbcbdf7c8c757487b1a8d97cd6b28ba7161
igorcesarcode <igorcesarbernades@gmail.com>
2018-11-02 21:39:01 -0300

    Adicionado configuração de porta de ambiente dinamica

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require ('express')
 const app = express()
 
+//Pegando a porta do ambinte caso não consiga ela inicia na porta 3000
+const port = process.env.PORT  || 3000
 
 app.get('/', (request, response) => {
     response.send ('<h1>Olá Fullstack Lab<h1>')
 })
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
     if (err){
         console.log('erro')
     }else {
-        console.log ('Como fazer server is running...')
+        console.log ('Servidor Iniciado na porta:',port)
     }
 }) 
