38ee157f4b378f3ee5e35f2463a7b9162843ce2c
igorcesarcode <igorcesarbernades@gmail.com>
2018-11-03 01:16:28 -0300

    Adicionando o app.post no index

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require ('express')
 const app = express()
 const axios = require('axios')
+const BodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
+app.use(BodyParser.urlencoded())
 
 //Pegando a porta do ambinte caso não consiga ela inicia na porta 3000
 const port = process.env.PORT  || 3000
@@ -11,9 +13,20 @@ const port = process.env.PORT  || 3000
 
 app.get('/', async(request, response) => {
     const content = await axios.get('https://como-fazer-igorcesarcode.firebaseio.com/teste.json')
-    console.log(content.data)
+ 
     response.render('index',{ i:content.data })
 })
+
+
+app.get('/categorias/nova', (req, res) => {
+    res.render ('categorias/nova')
+})
+
+app.post('/categorias/nova', (req, res) => {
+    res.send(req.body)
+    console.log (req.body)
+})
+
 
 app.listen(port, (err) => {
     if (err){
