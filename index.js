70227b254d2fb73ba8b60269f6c361ea30d8b4a5
igorcesarcode <igorcesarbernades@gmail.com>
2018-11-02 22:49:47 -0300

    Importando informações do Firebase com do Axios atravez do json

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require ('express')
 const app = express()
+const axios = require('axios')
 
 app.set('view engine', 'ejs')
 
 //Pegando a porta do ambinte caso não consiga ela inicia na porta 3000
 const port = process.env.PORT  || 3000
 
-let i = 10
 
-app.get('/', (request, response) => {
-    i++
-    response.render('index',{ i:i })
+
+app.get('/', async(request, response) => {
+    const content = await axios.get('https://como-fazer-igorcesarcode.firebaseio.com/teste.json')
+    console.log(content.data)
+    response.render('index',{ i:content.data })
 })
 
 app.listen(port, (err) => {
