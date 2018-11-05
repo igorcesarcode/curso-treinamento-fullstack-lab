959d4d2aec0397badcb69d137acdf59ffa1c4ba1
igorcesarcode <igorcesarbernades@gmail.com>
2018-11-05 21:08:07 -0200

    Foi adicionado os controllrs e routes no projeto para melhor organização

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require ('express')
 const app = express()
 const BodyParser = require('body-parser')
 
-const api = require('./api')
+const categorias = require('./routes/categorias.js')
+
 
 app.set('view engine', 'ejs')
 app.use(BodyParser.urlencoded())
@@ -10,56 +11,10 @@ app.use(BodyParser.urlencoded())
 //Pegando a porta do ambinte caso não consiga ela inicia na porta 3000
 const port = process.env.PORT  || 3000
 
-
 app.get('/', async(request, response) => {
     const content = await axios.get('https://como-fazer-igorcesarcode.firebaseio.com/teste.json')
  
     response.render('index',{ i:content.data })
-})
-
-
-app.get('/categorias/nova', (req, res) => {
-    res.render ('categorias/nova')
-})
-
-app.post('/categorias/nova', async(req, res) => {
-   await api.create('categorias',{
-        categoria:req.body.categoria
-    })
-    res.redirect('/categorias')
-    
-})
-
-//estar pegandos o objetos da api
-app.get('/categorias', async (req,res) => {
-      const categorias = await api.list ('categorias')
-            res.render('categorias/index', { categorias })
-        
-})
-
-app.get('/categorias/excluir/:id', async(req,res) =>{
-   await api.apagar('categorias',req.params.id)
-    res.redirect('/categorias')
-})
-
-
-
-
-app.get('/categorias/editar/:id', async(req, res) => {
-    const categoria = await api.get('categorias', req.params.id)
-    res.render('categorias/editar',{
-        categoria
-    } )
-
-    
-})
-
-app.post('/categorias/editar/:id', async(req, res) => {
-    await api.update('categorias',req.params.id, {
-        categoria: req.body.categoria
-    })
-    res.redirect('/categorias') 
-    
 })
 
 
@@ -70,3 +25,4 @@ app.listen(port, (err) => {
         console.log ('Servidor Iniciado na porta:',port)
     }
 }) 
+app.use('/categorias', categorias)
